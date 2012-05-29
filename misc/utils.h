41356fa65d9f71058306cdf731cd64b97c9e3ce2
Patrick Esser <PEsser@igpm.rwth-aachen.de>
2012-05-29 18:13:28 +0200

    ** GCC_VERSION definiert

--- misc/utils.h
+++ misc/utils.h
@@ -84,6 +84,14 @@
 double cbrt(double arg);
 #endif
 
+#ifdef __GNUC__
+#define GCC_VERSION (__GNUC__ * 10000 \
+                     + __GNUC_MINOR__ * 100 \
+                     + __GNUC_PATCHLEVEL__)
+#else
+#define GCC_VERSION 0
+#endif
+
 #include <cstddef>
 
 namespace DROPS
