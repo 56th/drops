481c43fc0ee2d51482ffe0ba5416e1d15c9468a1
Jens Berger <berger@igpm.rwth-aachen.de>
2013-04-24 17:17:26 +0200

    * Modifications for the testsuite (better descriptions for grabbing, no changes concerning content/calculations etc.)

--- src/tests/minres.cpp
+++ src/tests/minres.cpp
@@ -39,7 +39,7 @@ class TrivialPreCL
 
 int TestLanczos()
 {
-    std::cout << "Lanczos:\n" << std::endl;
+    std::cout << "TEST1 - METHOD: Lanczos" << std::endl;
     DROPS::MatrixCL A;
     DROPS::MatrixBuilderCL AB(&A, 2, 2);
     AB( 0, 1)= 1.; AB( 1, 0)= 1.;
@@ -47,7 +47,7 @@ int TestLanczos()
     DROPS::VectorCL r0( 1., 2);
     r0[0]= 1.; r0[1]= 2.;
 
-    std::cout << "A\n" << A << "r0\n" << r0 << std::endl;
+    std::cout << "A = " << A << '\n' << "r0 = " << r0 << std::endl;
     DROPS::LanczosONBCL<DROPS::VectorCL> onb;
     onb.new_basis(A, r0, DROPS::DummyExchangeCL());
     std::vector<DROPS::VectorCL> basis;
@@ -67,12 +67,13 @@ int TestLanczos()
         }
         std::cout << std::endl;
     }
+    std::cout  << '\n';
     return 0;
 }
 
 int TestMinres()
 {
-    std::cout << "Minres 2x2:\n" << std::endl;
+    std::cout << '\n' << "TEST2 - METHOD: Minres, 2x2 example" << std::endl;
     DROPS::MatrixCL A;
     DROPS::MatrixBuilderCL AB(&A, 2, 2);
     AB( 0, 1)= 1.; AB( 1, 0)= 1.;
@@ -81,38 +82,25 @@ int TestMinres()
     b[0]= 1.; b[1]= 2.;
     DROPS::VectorCL x( 0., 2);
 
-    std::cout << "A\n" << A << "b\n" << b << std::endl;
+    std::cout << "A =" << A  << '\n' << "b = " << b << std::endl;
     int mi= 10;
     double tol= 1e-10;
+    std::cout << "Minres: " ;
     MINRES( A, x, b, DROPS::DummyExchangeCL(), mi, tol);
-    std::cout << x << DROPS::VectorCL( A*x - b) << '\n' << mi << '\n' << tol << std::endl;
+    std::cout << "TEST2 x: " << x << "TEST2 Ax-b: " << DROPS::VectorCL( A*x - b) << '\n'  << "TEST2 Iterationnumber: " << mi
+              << '\n'<< "TEST2 Residual: "  << tol << '\n'  << std::endl;
     return 0;
 }
 
 int TestMinres2()
 {
-    std::cout << "Minres 4x4:\n" << std::endl;
+    std::cout << '\n' << "TEST3 - METHOD: Minres, 4x4 example" << std::endl;
     DROPS::MatrixCL A;
     DROPS::MatrixBuilderCL AB(&A, 4, 4);
-    AB( 0, 0)= -249.;
-    AB( 0, 1)= -453.;
-    AB( 0, 2)= -397.;
-    AB( 0, 3)= -52.;
-
-    AB( 1, 0)= -453.;
-    AB( 1, 1)= -731.;
-    AB( 1, 2)= -601.;
-    AB( 1, 3)= -78.;
-
-    AB( 2, 0)= -397.;
-    AB( 2, 1)= -601.;
-    AB( 2, 2)= -648.;
-    AB( 2, 3)= -91.;
-
-    AB( 3, 0)= -52.;
-    AB( 3, 1)= -78.;
-    AB( 3, 2)= -91.;
-    AB( 3, 3)= -13.;
+    AB( 0, 0)= -249.;AB( 1, 0)= -453.;AB( 2, 0)= -397.;AB( 3, 0)= -52.;
+    AB( 0, 1)= -453.;AB( 1, 1)= -731.;AB( 2, 1)= -601.;AB( 3, 1)= -78.;
+    AB( 0, 2)= -397.;AB( 1, 2)= -601.;AB( 2, 2)= -648.;AB( 3, 2)= -91.;
+    AB( 0, 3)=  -52.;AB( 1, 3)=  -78.;AB( 2, 3)=  -91.;AB( 3, 3)= -13.;
     AB.Build();
     DROPS::VectorCL b( 0., 4);
     b[0]= -1277./2/1.;
@@ -121,38 +109,25 @@ int TestMinres2()
     b[3]= -533./4.;
     DROPS::VectorCL x( 0., 4);
 
-    std::cout << "A\n" << A << "b\n" << b << std::endl;
+    std::cout << "A = " << A  << '\n' << "b = " << b << std::endl;
     int mi= 10;
     double tol= 1e-10;
+    std::cout << "Minres2: "; 
     MINRES( A, x, b, DROPS::DummyExchangeCL(), mi, tol);
-    std::cout << x << DROPS::VectorCL( A*x - b) << '\n' << mi << '\n' << tol << std::endl;
+    std::cout << "TEST3 x: " << x << "TEST3 Ax-b: " << DROPS::VectorCL( A*x - b) << '\n'  << "TEST3 Iterationnumber: " << mi
+              << '\n'<< "TEST3 Residual: "  << tol << '\n'  << std::endl;
     return 0;
 }
 
 int TestPMinres()
 {
-    std::cout << "PMinres 4x4:\n" << std::endl;
+    std::cout << '\n' << "TEST4 - METHOD: PMinres, 4x4 example" << std::endl;
     DROPS::MatrixCL A;
     DROPS::MatrixBuilderCL AB(&A, 4, 4);
-    AB( 0, 0)= -249.;
-    AB( 0, 1)= -453.;
-    AB( 0, 2)= -397.;
-    AB( 0, 3)= -52.;
-
-    AB( 1, 0)= -453.;
-    AB( 1, 1)= -731.;
-    AB( 1, 2)= -601.;
-    AB( 1, 3)= -78.;
-
-    AB( 2, 0)= -397.;
-    AB( 2, 1)= -601.;
-    AB( 2, 2)= -648.;
-    AB( 2, 3)= -91.;
-
-    AB( 3, 0)= -52.;
-    AB( 3, 1)= -78.;
-    AB( 3, 2)= -91.;
-    AB( 3, 3)= -13.;
+    AB( 0, 0)= -249.;AB( 1, 0)= -453.;AB( 2, 0)= -397.;AB( 3, 0)= -52.;
+    AB( 0, 1)= -453.;AB( 1, 1)= -731.;AB( 2, 1)= -601.;AB( 3, 1)= -78.;
+    AB( 0, 2)= -397.;AB( 1, 2)= -601.;AB( 2, 2)= -648.;AB( 3, 2)= -91.;
+    AB( 0, 3)=  -52.;AB( 1, 3)=  -78.;AB( 2, 3)=  -91.;AB( 3, 3)= -13.;
     AB.Build();
     DROPS::VectorCL b( 0., 4);
     b[0]= -1277./2/1.;
@@ -161,41 +136,28 @@ int TestPMinres()
     b[3]= -533./4.;
     DROPS::VectorCL x( 0., 4);
 
-    std::cout << "A\n" << A << "b\n" << b << std::endl;
+    std::cout << "A = " << A  << '\n' << "b = " << b << std::endl;
     int mi= 10;
     double tol= 1e-10;
     DROPS::LanczosONBCL<DROPS::VectorCL> q;
     q.new_basis( A, b, DROPS::DummyExchangeCL());
     DROPS::PMResSolverCL<DROPS::LanczosONBCL<DROPS::VectorCL> > pmr( q, mi, tol);
+    std::cout << "PMinres: "; 
     pmr.Solve( A, x, b, DROPS::DummyExchangeCL());
-    std::cout << x << DROPS::VectorCL( A*x - b) << '\n' << pmr.GetIter() << '\n' << pmr.GetResid() << std::endl;
+    std::cout << "TEST4 x: " << x << "TEST4 Ax-b: " << DROPS::VectorCL( A*x - b) << '\n' << "TEST4 Iterationnumber: " << pmr.GetIter() 
+              << '\n' << "TEST4 Residual: " << pmr.GetResid() << '\n' << std::endl;
     return 0;
 }
 
 int TestPMinres2()
 {
-    std::cout << "PMinres2 4x4:\n" << std::endl;
+    std::cout << '\n' << "TEST5 - METHOD: PMinres2, 4x4 example" << std::endl;
     DROPS::MatrixCL A;
     DROPS::MatrixBuilderCL AB(&A, 4, 4);
-    AB( 0, 0)= -249.;
-    AB( 0, 1)= -453.;
-    AB( 0, 2)= -397.;
-    AB( 0, 3)= -52.;
-
-    AB( 1, 0)= -453.;
-    AB( 1, 1)= -731.;
-    AB( 1, 2)= -601.;
-    AB( 1, 3)= -78.;
-
-    AB( 2, 0)= -397.;
-    AB( 2, 1)= -601.;
-    AB( 2, 2)= -648.;
-    AB( 2, 3)= -91.;
-
-    AB( 3, 0)= -52.;
-    AB( 3, 1)= -78.;
-    AB( 3, 2)= -91.;
-    AB( 3, 3)= -13.;
+    AB( 0, 0)= -249.;AB( 1, 0)= -453.;AB( 2, 0)= -397.;AB( 3, 0)= -52.;
+    AB( 0, 1)= -453.;AB( 1, 1)= -731.;AB( 2, 1)= -601.;AB( 3, 1)= -78.;
+    AB( 0, 2)= -397.;AB( 1, 2)= -601.;AB( 2, 2)= -648.;AB( 3, 2)= -91.;
+    AB( 0, 3)=  -52.;AB( 1, 3)=  -78.;AB( 2, 3)=  -91.;AB( 3, 3)= -13.;
     AB.Build();
     DROPS::VectorCL b( 0., 4);
     b[0]= -1277./2/1.;
@@ -204,14 +166,16 @@ int TestPMinres2()
     b[3]= -533./4.;
     DROPS::VectorCL x( 0., 4);
 
-    std::cout << "A\n" << A << "b\n" << b << std::endl;
+    std::cout << "A = " << '\n' << A << "b = " << b << std::endl;
     int mi= 10;
     double tol= 1e-10;
     DROPS::DummyPcCL pc;
     DROPS::PLanczosONBCL<DROPS::VectorCL, DROPS::DummyPcCL> q( pc);
     DROPS::PMResSolverCL<DROPS::PLanczosONBCL<DROPS::VectorCL, DROPS::DummyPcCL> > pmr( q, mi, tol);
+    std::cout << "PMinres2: "; 
     pmr.Solve( A, x, b, DROPS::DummyExchangeCL());
-    std::cout << x << DROPS::VectorCL( A*x - b) << '\n' << pmr.GetIter() << '\n' << pmr.GetResid() << std::endl;
+    std::cout << "TEST5 x: " << x << "TEST5 Ax-b: " << DROPS::VectorCL( A*x - b) << '\n' << "TEST5 Iterationnumber: " << pmr.GetIter() 
+              << '\n' << "TEST5 Residual: " << pmr.GetResid() << '\n' << std::endl;
     return 0;
 }
 
