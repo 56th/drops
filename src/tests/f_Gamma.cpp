c2d6643e8924a554bc9a1f9c543d8e07c8494d66
Joerg Grande <grande@igpm.rwth-aachen.de>
2013-10-02 16:31:00 +0200

    *Testfall mit variabler Grenzflaechenspannung. *SphereObliqueLaplaceBeltramiAccuCL: Akkumulator, der fuer eine Kugel  eine O(h)-Approximation der kontinuierlichen Grenzflaechenspannung als Vergleich berechnet.

--- src/tests/f_Gamma.cpp
+++ src/tests/f_Gamma.cpp
@@ -45,7 +45,11 @@ std::auto_ptr<DROPS::VTKOutCL> vtkwriter;
 int FctCode=9;
 
 double SurfTension;
-double sigmaf( const DROPS::Point3DCL&, double) { return SurfTension; }
+double sigmaf( const DROPS::Point3DCL& p, double)
+{
+//     return SurfTension;
+    return 1 + std::cos( 2.*M_PI*p[0]);
+}
 
 /*
 double DistanceFct( const DROPS::Point3DCL& p)
@@ -64,12 +68,18 @@ double DistanceFct( const DROPS::Point3DCL& p)
 
 */
 
+DROPS::Point3DCL c;
+double r;
 double DistanceFct( const DROPS::Point3DCL& p, double)
 { // ball
-    static const DROPS::Point3DCL c= P.get<DROPS::Point3DCL>("Exp.PosDrop");
-    static const double r= P.get<DROPS::Point3DCL>("Exp.RadDrop")[0];
-    return (p-c).norm()-r;
+//     return (p-c).norm()-r;
+    return inner_prod( p - c, p - c) - r*r;
 }
+
+// double DistanceFct( const DROPS::Point3DCL& p, double)
+// { // plane... XXX The line integral on \pa\iface must be implemented.
+//     return p[2] - 1./3.;
+// }
 
 DROPS::Point3DCL Null( const DROPS::Point3DCL&, double)
 { return DROPS::Point3DCL(0.); }
@@ -320,6 +330,152 @@ void Compare_LaplBeltramiSF_ConstSF( InstatStokes2PhaseP2P1CL& Stokes, const Lse
     delete &lset;
 }
 
+
+
+/// \brief Accumulator for the interfacial tension term with variable interfacial tension coefficient.
+class SphereObliqueLaplaceBeltramiAccuCL : public TetraAccumulatorCL
+{
+  private:
+    VecDescCL& f;
+    const LevelsetP2CL& ls_;
+    LocalNumbP2CL n_;
+
+    const PrincipalLatticeCL& lat_;
+    LocalP2CL<> loc_ls_;
+    std::valarray<double> ls_val_;
+    SurfacePatchCL p_;
+    QuadDomain2DCL q_;
+
+    LocalP1CL<Point3DCL> gradref_[10],
+                         grad_[10];
+    GridFunctionCL<Point3DCL> w_[10],
+                              qnt_,
+                              qnh_;
+    SMatrixCL<3, 3> T_;
+    GridFunctionCL<double> qsigma;
+
+    void
+    resize_and_scatter_piecewise_spatial_normal (const SPatchCL<3>& surf, const QuadDomainCodim1CL<3>& qdom, std::valarray<Point3DCL>& spatial_normal);
+   template <class ResultIteratorT>
+      void
+      evaluate_sigma_on_vertexes (const TetraCL& tet, const QuadDomain2DCL& q, double t, ResultIteratorT res);
+
+
+  public:
+    SphereObliqueLaplaceBeltramiAccuCL (const LevelsetP2CL& ls, VecDescCL& f_Gamma)
+     : f( f_Gamma), ls_( ls), lat_( PrincipalLatticeCL::instance( 2)),
+       ls_val_( 10)
+    {
+        P2DiscCL::GetGradientsOnRef( gradref_);
+    }
+
+    void begin_accumulation () {
+        f.Data= 0.;
+        std::cerr << "SphereObliqueLaplaceBeltramiAccuCL::begin_accumulation: " << f.Data.size() << "dof.\n";
+    }
+    void finalize_accumulation() {
+        std::cerr << "SphereObliqueLaplaceBeltramiAccuCL::finalize_accumulation.\n";
+    }
+
+    void visit (const TetraCL&);
+
+    TetraAccumulatorCL* clone (int /*tid*/) { return new SphereObliqueLaplaceBeltramiAccuCL ( *this); };
+};
+
+void
+SphereObliqueLaplaceBeltramiAccuCL::resize_and_scatter_piecewise_spatial_normal (const SPatchCL<3>& surf, const QuadDomainCodim1CL<3>& qdom, std::valarray<Point3DCL>& spatial_normal)
+{
+    spatial_normal.resize( qdom.vertex_size());
+    if (spatial_normal.size() == 0)
+        return;
+    if (surf.normal_empty()) // As qdom has vertexes, the must be facets, i.e. normals.
+        throw DROPSErrCL( "resize_and_scatter_piecewise_spatial_normal: normals were not precomputed.\n");
+
+    const Uint NodesPerFacet= qdom.vertex_size()/surf.facet_size();
+    if (qdom.vertex_size()%surf.facet_size() != 0)
+        throw DROPSErrCL( "resize_and_scatter_piecewise_spatial_normal: qdom.vertex_size is not a multiple of surf.facet_size.\n");
+
+    const SPatchCL<3>::const_normal_iterator n= surf.normal_begin();
+    for (Uint i= 0; i < surf.facet_size(); ++i) {
+        std::fill_n( &spatial_normal[i*NodesPerFacet], NodesPerFacet, n[i]);
+    }
+}
+
+template <class ResultIteratorT>
+void SphereObliqueLaplaceBeltramiAccuCL::evaluate_sigma_on_vertexes (const TetraCL& tet, const QuadDomain2DCL& q, double t, ResultIteratorT res)
+{
+    Bary2WorldCoordCL mapper( tet);
+    for (typename QuadDomain2DCL::const_vertex_iterator v= q.vertex_begin(), e= q.vertex_end(); v != e; ++v) {
+        const Point3DCL& p= mapper( *v);
+        *res++= sigmaf( (r/p.norm())*p, t);
+    }
+}
+
+void SphereObliqueLaplaceBeltramiAccuCL::visit (const TetraCL& t)
+{
+    evaluate_on_vertexes( ls_.GetSolution(), t, lat_, Addr( ls_val_));
+    if (equal_signs( ls_val_))
+        return;
+
+    loc_ls_.assign( t, ls_.GetSolution());
+    double det; // dummy
+    SMatrixCL<3,3> T;
+    GetTrafoTr( T, det, t);
+    P2DiscCL::GetGradients( grad_, gradref_, T);
+    LocalP1CL<Point3DCL> nt; // gradient of quadratic level set function
+    for (Uint i= 0; i < 10 ; ++i)
+        nt+= grad_[i]*loc_ls_[i];
+
+    p_.make_patch<MergeCutPolicyCL>( lat_, ls_val_);
+    p_.compute_normals( t);
+    make_CompositeQuad5Domain2D( q_, p_, t);
+    resize_and_scatter_piecewise_spatial_normal( p_, q_, qnh_); // unit-length normal to linear interface
+    resize_and_evaluate_on_vertexes( nt, q_, qnt_); // normal to quadratic interface
+    for (Uint i= 0; i < q_.vertex_size(); ++i)
+        qnt_[i]/= norm( qnt_[i]);
+    GridFunctionCL<> qalpha( dot( qnh_, qnt_));
+    // If a triangle has zero area, its normal is returned as 0; we avoid 
+    // division by zero... the value will not matter later as the func-det in quad_2D is also 0.
+    for (size_t i=0; i < qalpha.size(); ++i)
+        if (std::fabs( qalpha[i]) == 0.)
+            qalpha[i]= 1.;
+
+    // \alpha (1+d/R)
+    GridFunctionCL<double> qf;
+    resize_and_evaluate_on_vertexes( DistanceFct, t, q_, 0.,  qf);
+    qf/= r;
+    qf+= 1.;
+    qf*=std::abs( qalpha); // For the projectors, the orientation of the normals cancels, but not here.
+
+    GridFunctionCL<Point3DCL> qgradi( q_.vertex_size());
+    for (Uint i= 0; i < 10; ++i) {
+        evaluate_on_vertexes( grad_[i], q_, Addr( qgradi));
+        w_[i].resize( q_.vertex_size());
+        w_[i]= qgradi - GridFunctionCL<double>( dot( qnt_, qgradi)/qalpha)*qnh_; // \bQt D b^i, where b^i is the ith scalar P2-basis-function.
+    }
+    qsigma.resize( q_.vertex_size());
+    this->evaluate_sigma_on_vertexes( t, q_, /*time*/ 0., Addr( qsigma)); // interfacial tension
+
+    n_.assign_indices_only( t, *f.RowIdx);
+    for (Uint i= 0; i < 10; ++i) {
+        const GridFunctionCL<double>& tmp= GridFunctionCL<>( qsigma/qf);
+        add_to_global_vector( f.Data, -quad_2D(tmp*w_[i], q_), n_.num[i]);
+    }
+}
+
+void LSInit (const DROPS::MultiGridCL& mg, DROPS::VecDescCL& ls, double (*d)(const Point3DCL&, double), double t= 0.)
+{
+    const DROPS::Uint lvl= ls.GetLevel(),
+                      idx= ls.RowIdx->GetIdx();
+
+    DROPS_FOR_TRIANG_CONST_VERTEX( mg, lvl, it)
+        ls.Data[it->Unknowns( idx)]= d( it->GetCoord(), t);
+
+    DROPS_FOR_TRIANG_CONST_EDGE( mg, lvl, it)
+        ls.Data[it->Unknowns( idx)]= d( 0.5*(it->GetVertex( 0)->GetCoord() + it->GetVertex( 1)->GetCoord()), t);
+    ls.t= t;
+}
+
 void Compare_Oblique_Improved (DROPS::AdapTriangCL& adap, InstatStokes2PhaseP2P1CL& Stokes, LevelsetP2CL& lset)
 {
     MultiGridCL& MG= Stokes.GetMG();
@@ -341,7 +497,7 @@ void Compare_Oblique_Improved (DROPS::AdapTriangCL& adap, InstatStokes2PhaseP2P1
     std::cout << "Volumen = " << Vol << "\tKruemmung = " << curv << "\n\n";
 
     VecDescCL f_improved( vidx);
-    lset.SetSurfaceForce( SF_ImprovedLB);
+    lset.SetSurfaceForce( SF_ImprovedLBVar);
     lset.AccumulateBndIntegral( f_improved);
 
     VecDescCL f_oblique( vidx);
@@ -349,8 +505,78 @@ void Compare_Oblique_Improved (DROPS::AdapTriangCL& adap, InstatStokes2PhaseP2P1
     lset.AccumulateBndIntegral( f_oblique);
 
     vtkwriter->Register( make_VTKScalar( lset.GetSolution(), "Levelset") );
-    vtkwriter->Register( make_VTKVector( make_P2Eval( MG, Stokes.GetBndData().Vel, f_oblique),      "f_oblique"));
-    vtkwriter->Register( make_VTKVector( make_P2Eval( MG, Stokes.GetBndData().Vel, f_improved),     "f_improved"));
+    vtkwriter->Register( make_VTKVector( make_P2Eval( MG, Stokes.GetBndData().Vel, f_oblique),  "f_oblique"));
+    vtkwriter->Register( make_VTKVector( make_P2Eval( MG, Stokes.GetBndData().Vel, f_improved), "f_improved"));
+    BndDataCL<> nobnd( 0);
+    VecDescCL vd_sigmaf( &lset.idx);
+    LSInit( MG, vd_sigmaf, sigmaf, 0.);
+    vtkwriter->Register( make_VTKScalar( make_P2Eval( MG, nobnd, vd_sigmaf),                    "sigmaf"));
+    vtkwriter->Write( 0.);
+
+    VectorCL d( f_oblique.Data - f_improved.Data);
+    std::cout << "|d| = \t\t" << norm(d) << std::endl;
+// std::cerr << "oblique: " << f_oblique.Data.size() << ", improved: " << f_improved.Data.size() << std::endl;
+// for (size_t i= 0; i < f_oblique.Data.size(); ++i)
+//     if (isnan( f_oblique.Data[i]))
+//         std::cerr << "i: " << i << std::endl;
+// for (size_t i= 0; i < f_improved.Data.size(); ++i)
+//     if (isnan( f_improved.Data[i]))
+//         std::cerr << "Hallo i: " << i << std::endl;
+// for (size_t i= 0; i < d.size(); ++i)
+//     if (isnan( d[i]))
+//         std::cerr << "Hallo d i: " << i << std::endl;
+
+    Stokes.SetupSystem1( &Stokes.A, &Stokes.M, &Stokes.b, &Stokes.b, &Stokes.b, lset, 0.);
+    MLMatrixCL MA;
+    MA.LinComb( 1, Stokes.M.Data, 1, Stokes.A.Data);
+    VectorCL MA_inv_d( d);
+    std::cout << "Solving system with MA matrix:\t";
+    SSORPcCL pc;
+    typedef PCGSolverCL<SSORPcCL> PCG_SsorCL;
+    PCG_SsorCL cg( pc, 1000, 1e-18);
+    cg.Solve( MA, MA_inv_d, d, vidx->GetEx());
+    std::cout << cg.GetIter() << " iter,\tresid = " << cg.GetResid();
+    const double sup2= std::sqrt(dot( MA_inv_d, d));
+    std::cout << "\n\nsup |f1(v)-f2(v)|/||v||_1 = \t\t" << sup2
+              << "\n|(MA)^-1 d| = " << norm( MA_inv_d) << std::endl;
+}
+
+void Compare_Oblique_Helper (DROPS::AdapTriangCL& adap, InstatStokes2PhaseP2P1CL& Stokes, LevelsetP2CL& lset)
+{
+    MultiGridCL& MG= Stokes.GetMG();
+
+    MLIdxDescCL* vidx= &Stokes.vel_idx;
+    Stokes.CreateNumberingVel( MG.GetLastLevel(), vidx);
+
+    MG.SizeInfo( std::cout);
+    Stokes.b.SetIdx( vidx);
+    Stokes.v.SetIdx( vidx);
+    Stokes.A.SetIdx( vidx, vidx);
+    Stokes.M.SetIdx( vidx, vidx);
+
+    std::cout << vidx->NumUnknowns() << " velocity unknowns,\n";
+    std::cout << lset.Phi.Data.size() << " levelset unknowns.\n";
+
+    const double Vol= 4./3*M_PI*std::pow( P.get<DROPS::Point3DCL>("Exp.RadDrop")[0], 3);
+    const double curv= 2./P.get<DROPS::Point3DCL>("Exp.RadDrop")[0];
+    std::cout << "Volumen = " << Vol << "\tKruemmung = " << curv << "\n\n";
+
+    VecDescCL f_improved( vidx);
+    TetraAccumulatorTupleCL accus;
+    accus.push_back_acquire( new SphereObliqueLaplaceBeltramiAccuCL( lset, f_improved));
+    accumulate( accus, MG, vidx->TriangLevel(), vidx->GetMatchingFunction(), vidx->GetBndInfo());
+
+    VecDescCL f_oblique( vidx);
+    lset.SetSurfaceForce( SF_ObliqueLBVar);
+    lset.AccumulateBndIntegral( f_oblique);
+
+    vtkwriter->Register( make_VTKScalar( lset.GetSolution(), "Levelset") );
+    vtkwriter->Register( make_VTKVector( make_P2Eval( MG, Stokes.GetBndData().Vel, f_oblique),  "f_oblique"));
+    vtkwriter->Register( make_VTKVector( make_P2Eval( MG, Stokes.GetBndData().Vel, f_improved), "f_improved"));
+    BndDataCL<> nobnd( 0);
+    VecDescCL vd_sigmaf( &lset.idx);
+    LSInit( MG, vd_sigmaf, sigmaf, 0.);
+    vtkwriter->Register( make_VTKScalar( make_P2Eval( MG, nobnd, vd_sigmaf),                    "sigmaf"));
     vtkwriter->Write( 0.);
 
     VectorCL d( f_oblique.Data - f_improved.Data);
@@ -407,6 +633,8 @@ int main (int argc, char** argv)
     std::cout << P << std::endl;
 
     SurfTension= P.get<double>( "SurfTens.SurfTension");
+    c= P.get<DROPS::Point3DCL>("Exp.PosDrop");
+    r= P.get<DROPS::Point3DCL>("Exp.RadDrop")[0];
 
     DROPS::dynamicLoad(P.get<std::string>("General.DynamicLibsPrefix"), P.get<std::vector<std::string> >("General.DynamicLibs") );
 
@@ -450,7 +678,8 @@ int main (int argc, char** argv)
         P.get<bool>("VTK.ReUseTimeFile")
     ));
 //     Compare_LaplBeltramiSF_ConstSF( prob, lsbnd);
-    Compare_Oblique_Improved( adap, prob, lset);
+//     Compare_Oblique_Improved( adap, prob, lset);
+    Compare_Oblique_Helper( adap, prob, lset);
 
     delete &lset;
     return 0;
