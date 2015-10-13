4bf03c44486397731de2d7b794ba92a4f966a261
Christoph Lehrenfeld <schrustup@gmail.com>
2015-10-13 12:09:36 +0200

    move some definition to header

--- src/levelset/marking_strategy.h
+++ src/levelset/marking_strategy.h
@@ -201,6 +201,50 @@ private:
     MarkingDecisionT decision_;
 };
 
+
+///////////////////////////////////////////
+// ValueGetter for DistMarkingStrategyCL //
+///////////////////////////////////////////
+
+class ValueGetterCL
+{
+public:
+    virtual ~ValueGetterCL() {}
+    virtual double GetValue( const VertexCL& v ) const = 0;
+    virtual double GetValue( const EdgeCL&   e ) const = 0;
+    virtual double GetValue( const TetraCL&  t ) const = 0;
+    virtual ValueGetterCL* clone() const = 0;
+};
+
+class LevelsetP2GetterCL: public ValueGetterCL
+{
+public:
+    LevelsetP2GetterCL( const LevelsetP2CL& lset );
+
+    double GetValue( const VertexCL& v ) const;
+    double GetValue( const EdgeCL&   e ) const;
+    double GetValue( const TetraCL&  t ) const;
+    ValueGetterCL* clone() const;
+
+private:
+    const LevelsetP2CL& lset_;
+};
+
+class FunPtrGetterCL: public ValueGetterCL
+{
+public:
+    FunPtrGetterCL( instat_scalar_fun_ptr fct, double time = 0. );
+
+    double GetValue( const VertexCL& v ) const;
+    double GetValue( const EdgeCL&   e ) const;
+    double GetValue( const TetraCL&  t ) const;
+    ValueGetterCL* clone() const;
+
+private:
+    double time_;
+    instat_scalar_fun_ptr fct_;
+};
+
 }
 
 #endif
