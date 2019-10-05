6680e574c3659a53c6535f3c6ee7d8f697307fe1
Alexander Zhiliakov <alex@compute00.e.math.uh.edu>
2019-10-05 16:24:19 -0500

    Add configure files for local and remote machines

--- do-configure-alex.sh
+++ do-configure-alex.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+mkdir bin
+cd bin
+
 cmake \
 	-D TRILINOS_PATH:FILEPATH="/home/alexander/Documents/Amanzi/amanzi/install/tpls/trilinos-12-12-1" \
 	-D Matlab_ROOT_DIR:FILEPATH="/usr/local/MATLAB/R2019a" \
