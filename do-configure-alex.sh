a279e8fcb1732fea3df49ff556df6ac8726fe3e8
Alexander Zhiliakov <alex@math.uh.edu>
2019-10-07 16:46:52 -0500

    Add vtk find_package

--- do-configure-alex.sh
+++ do-configure-alex.sh
@@ -4,6 +4,7 @@ mkdir bin
 cd bin
 
 cmake \
+	-D VTK_DIR:FILEPATH="/usr/lib/cmake/vtk-7.1" \
 	-D TRILINOS_PATH:FILEPATH="/home/alexander/Documents/Amanzi/amanzi/install/tpls/trilinos-12-12-1" \
 	-D Matlab_ROOT_DIR:FILEPATH="/usr/local/MATLAB/R2019a" \
 	-D DROPS_BUILD_TYPE:STRING="RELEASE" \
